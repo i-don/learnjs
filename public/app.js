b0eddab77795c85e67815874a3e5182f9c884f1a
i-don <t-ishi@ba2.so-net.ne.jp>
2018-07-15 21:58:17 +0900

    ３章やった

--- public/app.js
+++ public/app.js
@@ -1,20 +1,73 @@
 'use strict';
 var learnjs = {};
 
-learnjs.problemView = function(problemNumber) {
-  var title = 'Problem #' + problemNumber + ' Coming soon!'
-  return $('<div class="problem-view">').text(title);
+learnjs.landingView = function() {
+    return learnjs.template('landing-view');
+}
+
+learnjs.problemView = function(data) {
+  var problemNumber = parseInt(data, 10);
+  var view = $('.templates .problem-view').clone();
+  var problemData = learnjs.problems[problemNumber - 1];
+  var resultFlash = view.find('.result');
+
+  function checkAnswer() {
+    var answer = view.find('.answer').val();
+    var test =  problemData.code.replace('__', answer) + '; problem();';
+    return eval(test);
+  }
+
+  function checkAnswerClick() {
+    if (checkAnswer()) {
+      learnjs.flashElement(resultFlash, learnjs.buildCorrectFlash(problemNumber));
+    } else {
+      learnjs.flashElement(resultFlash, 'Incorrect!');
+    }
+    return false;
+  }
+
+  view.find('.check-btn').click(checkAnswerClick);
+  view.find('.title').text('Problem #' + problemNumber);
+  learnjs.applyObject(problemData, view);
+  if(problemNumber < learnjs.problems.length) {
+    var buttonItem = learnjs.template('skip-btn');
+    buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+    $('.nav-list').append(buttonItem);
+    view.bind('removingView', function() {
+      buttonItem.remove();
+    });
+  }
+  return view;
+}
+
+learnjs.buildCorrectFlash = function(problemNumber) {
+  var correctFlash = learnjs.template('correct-flash');
+  var link = correctFlash.find('a');
+  if (problemNumber < learnjs.problems.length) {
+    link.attr('href', '#problem-' + (problemNumber + 1));
+  } else {
+    link.attr('href', '');
+    link.text("You're Finished!");
+  }
+  return correctFlash;
 }
 
 learnjs.showView = function(hash) {
   var routes = {
-    '#problem':learnjs.problemView
+     '':learnjs.landingView
+    ,'#':learnjs.landingView
+    ,'#problem':learnjs.problemView
   };
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
   if (viewFn) {
+    learnjs.triggerEvent('removingView', [])
     $('.view-container').empty().append(viewFn(hashParts[1]));
   }
+}
+
+learnjs.triggerEvent = function(name, args) {
+  $('.view-container>*').trigger(name, args);
 }
 
 learnjs.appOnReady = function(hash) {
@@ -23,3 +76,35 @@ learnjs.appOnReady = function(hash) {
   }
   learnjs.showView(window.location.hash);
 }
+
+learnjs.applyObject = function(obj, elem) {
+  for (var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+};
+
+learnjs.flashElement = function(elem, content) {
+  elem.fadeOut('fast', function() {
+    elem.html(content);
+    elem.fadeIn();
+  });
+}
+
+learnjs.template = function(name) {
+  return $('.templates .' + name).clone();
+}
+
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  }
+  ,{
+    description: "Simple Math",
+    code: "function problem() { return 42 === 6 * __; }"
+  }
+  ,{
+    description: "Simple Math",
+    code: "function problem() { return 60 === 3 * __; }"
+  }
+];
